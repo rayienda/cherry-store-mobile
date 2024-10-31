9188e31d56f0c201f7e843da1e716fc346ba4ea6
RAYIENDA <rayienda18@gmail.com>
2024-10-31 11:04:42 +0700

    first commit

---
+++ lib/menu.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart'; 
+
+class MyHomePage extends StatelessWidget {
+  final String npm = '2306172735'; // NPM
+  final String name = 'Rayienda Hasmaradana Najlamahsa'; // Name
+  final String className = 'PBD'; // Class
+  final List<ItemHomepage> items = [
+         ItemHomepage("View Product", Icons.mood),
+         ItemHomepage("Add Product", Icons.add),
+         ItemHomepage("Logout", Icons.logout),
+  ];
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold provides the basic structure of the page with the AppBar and body.
+    return Scaffold(
+      // AppBar is the top part of the page that displays the title.
+      appBar: AppBar(
+        title: const Text(
+          'Cherry Store',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // The background color of the AppBar is obtained from the application theme color scheme.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      // Body of the page with paddings around it.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // Place the widget vertically in a column.
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Row to display 3 InfoCard horizontally.
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InfoCard(title: 'NPM', content: npm),
+                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'Class', content: className),
+              ],
+            ),
+
+            // Give a vertical space of 16 units.
+            const SizedBox(height: 16.0),
+
+            // Place the following widget in the center of the page.
+            Center(
+              child: Column(
+                // Place the text and grid item vertically.
+
+                children: [
+                  // Display the welcome message with bold font and size 18.
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Welcome to Cherry Store',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+
+                  // Grid to display ItemCard in a 3 column grid.
+                  GridView.count(
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    // To ensure that the grid fits its height.
+                    shrinkWrap: true,
+
+                    // Display ItemCard for each item in the items list.
+                    children: items.map((ItemHomepage item) {
+                      return ItemCard(item);
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  // Card information that displays the title and content.
+
+  final String title;  // Card title.
+  final String content;  // Card content.
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Create a card box with a shadow.
+      elevation: 2.0,
+      child: Container(
+        // Set the size and spacing within the card.
+        width: MediaQuery.of(context).size.width / 3.5, // Adjust with the width of the device used.
+        padding: const EdgeInsets.all(16.0),
+        // Place the title and content vertically.
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemHomepage {
+    final String name;
+    final IconData icon;
+
+    ItemHomepage(this.name, this.icon);
+}
+
+class ItemCard extends StatelessWidget {
+  // Display the card with an icon and name.
+
+  final ItemHomepage item; 
+  
+  const ItemCard(this.item, {super.key}); 
+
+    @override
+  Widget build(BuildContext context) {
+    // Assign different colors based on the item name
+    Color getButtonColor() {
+      switch (item.name) {
+        case "View Product":
+          return Colors.lightBlue; // Color for "View Product" button
+        case "Add Product":
+          return Colors.lightGreen; // Color for "Add Product" button
+        case "Logout":
+          return Colors.red; // Color for "Logout" button
+        default:
+          return Theme.of(context).colorScheme.secondary;
+      }
+    }
+
+    return Material(
+      color: getButtonColor(),
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("You have pressed the ${item.name} button!"))
+            );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
